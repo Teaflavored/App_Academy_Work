5cfc9f703e5399666fb89d3c3ec2dd5197581a51
Teaflavored <auster.s.chen@gmail.com>
2014-10-08 18:14:52 -0700

    changed name and deleted old hangman

--- W1D3/hangman.rb
+++ W1D3/hangman.rb
@@ -1,70 +1,87 @@
 class Hangman
-  
   def initialize(player1,player2)
-    @p1 = player1
-    @p2 = player2
-    @secret_letters = @p1.pick_word.split("")
-    @current_state = Array.new(@secret_letters.count) { "_" }
-  end
-  
-  def out_of_guesses?
-    @p2.tries == 0
+    @picking_player = player1 #player who picks the word
+    @guessing_player = player2 #player who tries to guess
+
   end
   
   def display
-    puts "Secret word: #{ @current_state.join('') }"
+    puts "Secret word: #{ @guessing_player.current_state.join('') }"
   end
   
   def add_to_player_guesses(user_input)
-    @p2.guess_letter(user_input)
-  end
-  
-  def update_current_state(user_input)
-    @secret_letters.each_with_index do |el, index|
-      if el == user_input
-        @current_state[index] = el
-      end
-    end
+    @guessing_player.guess_letter(user_input) 
   end
   
   def play
     puts "Welcome to Hangman!"
+    @guessing_player.set_word_length( @picking_player.pick_word )
     
     until game_over?
       display
-      guess = @p2.get_user_input
-      update_current_state(guess)
-      add_to_player_guesses(guess)
-      @p2.count_down_tries
+      temp_choice = @guessing_player.get_user_input
+      add_to_player_guesses(temp_choice)
+      @picking_player.check_letter_and_position(temp_choice, @guessing_player)
+      @guessing_player.update_current_state(@guessing_player)
     end
     
-    out_of_guesses? ? (puts "You lose, you're out of guesses, the answer was #{ @secret_letters.join("") }") : (puts "Congrats, you've solved the word \"#{ @secret_letters.join("") }\" in #{10-@tries} guesses") 
-    
+    @guessing_player.solved? ? (puts "Congrats #{ @guessing_player.name }, you've solved the word \"#{ @guessing_player.return_final_word }\" in #{15-@guessing_player.tries} guesses") : (puts "#{ @guessing_player.name }, you lose, you're out of guesses") 
   end
-  
-  private
-    
-    def game_over?
-      @current_state == @secret_letters || out_of_guesses?
-    end
+
+  def game_over?
+    !@guessing_player.tries_left? || @guessing_player.solved?
+  end
   
 end
 
 class Player
-  attr_reader :name, :word, :guesses, :tries
+  attr_reader :name, :word, :guesses, :tries, :current_state, :word
+  attr_accessor :correct_letters
   
   def initialize(name = "computer")
     @name = name
-    @word = nil
     @guesses = []
-    @tries = 10
-    @dictionary = load_dictionary
+    @tries = 15
+    @correct_letters = {}
+  end
+  
+  def set_word_length(word_length)
+    @current_state = Array.new(word_length) { "_" }
+  end
+  
+  def solved?
+    !@current_state.include?("_")
+  end
+  
+  def tries_left?
+    @tries > 0
+  end
+  
+  #update the current state
+  def update_current_state(player)
+    player.correct_letters.each do |key, values|
+      values.each do |index|
+        player.current_state[index] = key
+      end
+    end
   end
   
   def count_down_tries
     @tries -= 1
   end
   
+  def guess_letter(letter)
+    @guesses << letter
+  end
+  
+  def return_final_word
+    "#{ @current_state.join("") }"
+  end
+  
+  
+end
+
+class HumanPlayer < Player
   def get_user_input
     loop do
       puts "Guess a letter.  You have #{@tries} tries left."
@@ -74,53 +91,117 @@ class Player
     end
   end
   
+  def check_letter_and_position(letter,player)
+    puts "Is #{letter} in your word? #{player.tries} tries left"
+    player.count_down_tries
+    selection = gets.chomp.downcase
+    if selection == 'y'
+      puts "Where are the positions"
+      selection2 = gets.chomp.split(',').map(&:to_i)
+      player.correct_letters[letter] = selection2
+    end
+  end
+  
   def pick_word
     puts "What is the length of the word?"
     word_length = gets.chomp.to_i
-    @word = @dictionary.select{ |word| word.length == word_length }.sample
   end
   
-  def guess_letter(letter)
-    @guesses << letter
-  end
-  
-  def valid_input?(user_input)
-    user_input.match(/[a-z]{1}/) && user_input.length == 1
-  end
+  private 
+    def valid_input?(user_input)
+      user_input.match(/[a-z]{1}/) && user_input.length == 1
+    end
 
-  def okay_letter?(user_input)
-    return true if valid_input?(user_input) && !self.guesses.include?(user_input)
-    nil
+    def okay_letter?(user_input)
+      return true if valid_input?(user_input) && !self.guesses.include?(user_input)
+      nil
+    end
+end
+
+class CompPlayer < Player
+
+  def initialize 
+    super
+    @dictionary = load_dictionary
+    @word = pick_random_word_from_dictionary
+  end
+  
+  def pick_word
+    @word.length
+  end
+  
+  def get_user_input
+    update_smart_dictionary
+    update_smart_dictionary2
+    choice = most_frequent_letter
+    choice
+  end
+  
+  def update_smart_dictionary
+    dummy_copy = @smart_dictionary.dup
+    @smart_dictionary.each do |word| 
+      temp_arr = word.split('')
+      
+      @current_state.each_with_index do |char, index|
+        dummy_copy.delete(word) unless char == "_" || temp_arr[index] == char
+      end
+    end
+    @smart_dictionary = dummy_copy.dup
+  end
+  
+  def update_smart_dictionary2
+    dummy_copy = @smart_dictionary.dup
+    wrong_guesses = @guesses - @current_state
+    @smart_dictionary.reject! do |word|
+      wrong_guesses.each do |char|
+        dummy_copy.delete(word) if word.include?(char)
+      end
+    end
+    @smart_dictionary = dummy_copy.dup
+  end
+  
+  def most_frequent_letter
+    long_string = @smart_dictionary.join('')
+    counts = Hash.new(0)
+    long_string.split("").each do |char|
+      counts[char] += 1 unless @guesses.include?(char)
+    end
+    counts.max_by { |k, v| v }.first
+  end
+  
+  def set_word_length(word_length)
+    @current_state = Array.new(word_length) { "_" }
+    filter_number_of_words(word_length)
+  end
+  
+  def filter_number_of_words(word_length)
+    @smart_dictionary = @dictionary.select{|word| word.length == word_length }
+  end
+  
+  def check_letter_and_position(letter, player)
+    player.count_down_tries
+    indices = []
+    
+    @word.split("").each_with_index do |char, index|
+        indices << index if char == letter
+    end
+    player.correct_letters[letter] = indices
   end
   
   def load_dictionary
     File.readlines("dictionary.txt").map(&:chomp)
   end
   
-end
-
-class HumanPlayer < Player
-end
-
-class CompPlayer < Player
-
-  def pick_word
+  def pick_random_word_from_dictionary
     @dictionary.sample
   end
   
-  def get_user_input
-    alphabet = [*'a'..'z']
-    loop do
-      choice = alphabet.sample
-      p choice
-      return choice unless @guesses.include?(choice)
-    end
-  end
-  
 end
 
-
+#enrapture
 auster = HumanPlayer.new("Auster")
+justin = HumanPlayer.new("Justin")
 comp = CompPlayer.new
-test = Hangman.new(auster, comp)
+comp2 = CompPlayer.new
+test = Hangman.new(comp,comp2)
 test.play
