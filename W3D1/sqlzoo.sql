38ce1ae898988371f4680f76a7b3c07dc3a6d331
Auster Chen <Auster.s.chen@gmail.com>
2014-10-21 21:50:09 -0700

    more sqlzoo work

--- W3D1/sqlzoo.sql
+++ W3D1/sqlzoo.sql
@@ -264,6 +264,74 @@ WHERE subject = 'Chemistry') b
 ON a.yr = b.yr)
 WHERE b.yr IS NULL
 
+/* SUM AND COUNT */
+/* 1 */
+SELECT
+SUM(population)
+FROM
+world
+
+/* 2 */
+SELECT
+continent
+FROM
+world
+GROUP BY
+continent
+
+/* 3 */
+SELECT
+SUM(gdp)
+FROM 
+world
+WHERE
+continent = 'Africa'
+
+/* 4 */
+SELECT
+COUNT(name)
+FROM
+world
+WHERE
+area >= 1000000
+
+/* 5 */
+SELECT
+SUM(population)
+FROM
+world
+WHERE
+name IN ('France', 'Germany', 'Spain')
+
+/* 6 */
+SELECT
+continent, COUNT(name)
+FROM
+world
+GROUP BY
+continent
+
+/* 7 */
+SELECT
+continent, COUNT(name)
+FROM
+world
+WHERE
+population >= 10000000
+GROUP BY
+continent
+
+/* 8 */
+SELECT
+continent
+FROM
+world
+GROUP BY
+continent
+HAVING SUM(population) >= 100000000
+
+/* JOIN */
+
 
 SELECT
 e.mdate, e.team1, SUM(e.score1), e.team2, SUM(e.score2)
